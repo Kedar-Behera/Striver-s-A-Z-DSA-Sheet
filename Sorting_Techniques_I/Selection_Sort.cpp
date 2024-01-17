ae8d25bee56aec2ef95a946908ff699ce94534a0
kedar-19 <beherakedar506@gmail.com>
2024-01-17 12:30:13 +0530

    Selection Sort

---
+++ Sorting_Techniques_I/Selection_Sort.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+void selectionSort(vector<int>&arr) {
+    // Write your code here.
+    int n = arr.size();
+    for(int i = 0;i<=n-2;i++){
+        int mini = i;
+        for(int j = i;j<=n-1;j++){
+            if(arr[j]<arr[mini]){
+                mini = j;
+            }
+            
+        }
+        int temp = arr[i];
+                arr[i] = arr[mini];
+                arr[mini] = temp;
+    }
+}
