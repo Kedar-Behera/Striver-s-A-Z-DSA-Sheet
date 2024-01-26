f98eaf6cdb12b17482ceccf4b89b69d2d1e49710
kedar-19 <beherakedar506@gmail.com>
2024-01-26 18:45:49 +0530

    N to 1 without loop

---
+++ Learn the Basics/N_to_1_without_loop.cpp
@@ -0,0 +1,14 @@
+void helper(int x,vector<int>& ans){
+    if(x == 0){
+        return;
+    }
+    ans.push_back(x);
+    helper(x-1,ans);
+}
+
+vector<int> printNos(int x) {
+    // Write Your Code Here
+    vector<int>ans;
+    helper(x,ans);
+    return ans;
+}
