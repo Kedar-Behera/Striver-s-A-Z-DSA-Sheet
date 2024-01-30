918784609291f7e98e43607ed811281a00992457
kedar-19 <beherakedar506@gmail.com>
2024-01-30 17:08:13 +0530

    Check if a string is pallindrome or not

---
+++ Learn the Basics/Valid_Pallindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.size() == 0 || s.size() == 1){
+            return true;
+        }
+        int start = 0, end = s.length()-1;
+        while(start<end){
+            while(start<end && !isalnum(s[start])){
+                start++;
+            }
+            while(start<end && !isalnum(s[end])){
+                end--;
+            }
+            if(start<end && tolower(s[start]) != tolower(s[end])){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
